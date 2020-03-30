934edd4225b3a8f9d19dcb156222ca787c21ac55
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-03-30 21:18:19 +0300

    Showing ID instead of object reference

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,4 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :tickets, dependent: :destroy
   validates :user, :name, :location, presence: true
-  validates :user, uniqueness: true
 end
