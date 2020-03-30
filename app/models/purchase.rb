28a6d6d95718b7322062a156e9388beec63ea5f5
hl00901 <hl00901@surrey.ac.uk>
2020-03-30 17:22:01 +0100

    Need devise testing

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :ticket
+
+  validates :user, :ticket, :PriceTotal, presence: true
+  validates :user, :ticket, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/home'
   resources :purchases
   resources :tickets
   resources :events
   devise_for :users
-  root 'home#index'
+  root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
