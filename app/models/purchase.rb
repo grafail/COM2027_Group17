46deb9142b78ce5560eac4136ba0f8745771b65a
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-03-30 21:37:52 +0300

    Removed some restrictions

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,5 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :ticket
-
   validates :user, :ticket, :PriceTotal, presence: true
-  validates :user, :ticket, uniqueness: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,4 @@
 class Ticket < ApplicationRecord
   belongs_to :event
-  has_one :purchase, dependent: :destroy
   validates :event, :price, :name, presence: true
 end
