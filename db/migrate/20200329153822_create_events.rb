ad8c382983abb4172f1d677054d3ef02e8c1e7b0
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-03-30 21:18:50 +0300

    Removed unique atrribute requirement

--- db/migrate/20200329153822_create_events.rb
+++ db/migrate/20200329153822_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-      t.belongs_to :user, foreign_key: true, null: false, unique: true
+      t.belongs_to :user, foreign_key: true, null: false
       t.string :name, null: false
       t.text :description
       t.text :location, null: false
