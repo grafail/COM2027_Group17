db4db4a8f3b75ceb9c608887c5d7bd9eb07a5685
naiema <no00251@surrey.ac.uk>
2020-04-12 04:49:46 +0100

    contact page created

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,14 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+
+    assert_template layout: 'application'
+
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
+
 end
