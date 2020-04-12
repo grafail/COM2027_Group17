2e73a76ac49e6c398411c384eb3ede22ffd7ac97
Grace <gl00233@surrey.ac.uk>
2020-04-12 19:03:41 +0100

    revert

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_161419) do
+ActiveRecord::Schema.define(version: 2020_03_29_153824) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -53,14 +53,6 @@ ActiveRecord::Schema.define(version: 2020_04_11_161419) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "venues", force: :cascade do |t|
-    t.string "name"
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
