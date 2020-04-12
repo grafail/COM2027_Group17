ef7eb9c0ae1184df99183320274f3ac6da1f1253
Grace <gl00233@surrey.ac.uk>
2020-04-12 19:09:36 +0100

    Add latitude and longitude attribute to Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_153824) do
+ActiveRecord::Schema.define(version: 2020_04_12_180847) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_03_29_153824) do
     t.text "location", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
