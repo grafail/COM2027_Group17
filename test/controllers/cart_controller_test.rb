6c25d6bb8b7a55b1af3fff99aa1b00d9ce6722be
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-04-21 05:26:45 +0300

    Fixed test

--- test/controllers/cart_controller_test.rb
+++ test/controllers/cart_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CartControllerTest < ActionDispatch::IntegrationTest
   test "should get cart" do
-    get cart_cart_url
+    get cart_url
     assert_response :success
   end
 
