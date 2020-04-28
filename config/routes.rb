c8e40b1fb793a7b51ec0745df3dd3bd170a8d14e
naiema <no00251@surrey.ac.uk>
2020-04-28 01:51:46 +0100

    x

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
