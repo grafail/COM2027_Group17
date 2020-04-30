47ba42a1a176f3ccdced7a3e0634a996008548d8
naiema <no00251@surrey.ac.uk>
2020-04-30 16:05:32 +0100

    created the about us page

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
 
+  get 'about', to: 'home#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
