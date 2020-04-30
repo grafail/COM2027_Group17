19aeb61de1683345530fc8f718c193ff616be07a
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-04-30 06:35:32 +0300

    Fixed test

--- test/controllers/checkout_controller_test.rb
+++ test/controllers/checkout_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class CheckoutControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get checkout_index_url
-    assert_response :success
+    get checkout_url
+    assert_response :found
   end
 
 end
