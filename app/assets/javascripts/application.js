4ca514b3ab4ce1e848918ffdb36e558563e034ca
naiema <no00251@surrey.ac.uk>
2020-05-02 01:30:08 +0100

    made the header responsive, still needs work done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,3 +47,12 @@ $(document).ready(function() {
         "hideMethod": "fadeOut"
     }
 });
+
+function myFunction() {
+    var x = document.getElementById("myHeader");
+    if (x.className === "header") {
+        x.className += " responsive";
+    } else {
+        x.className = "header";
+    }
+}
