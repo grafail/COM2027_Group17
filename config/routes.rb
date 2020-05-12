981d406a15c08b35aa351d76d8d5516b4013422b
naiema <no00251@surrey.ac.uk>
2020-05-12 12:21:30 +0100

    created the termsandconditions page

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
 
   get 'about', to: 'home#about'
   get 'privacy', to: 'home#privacy'
+  get 'termsandconditions', to: 'home#termsandconditions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
