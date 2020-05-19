c835aef1bd9c559adfeb2d3b19dcce7ce2d8e8d2
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-05-19 04:35:41 +0300

    Added business user to seed

--- db/migrate/20200512181639_add_is_business_to_user.rb
+++ db/migrate/20200512181639_add_is_business_to_user.rb
@@ -1,5 +1,5 @@
 class AddIsBusinessToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :isBusiness, :boolean, null: false
+    add_column :users, :isBusiness, :boolean, null: false, :default => false
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 User.create!([
-               { email: "test@example.org", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil }
+               { email: "test@example.org", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil },
+               { email: "test2@example.org", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, isBusiness:true }
              ])
 Event.create!([
-                { user_id: 1, name: "Test Event 1", description: "This is a description.", eventDate: "2020-12-25 18:00:00", location: "Guildford", latitude: 51.2356068, longitude: -0.5732063, eventType: 1 },
-                { user_id: 1, name: "Test Event 2", description: "This is a description.", eventDate: "2020-12-26 18:00:00", location: "New York", latitude: 40.7127281, longitude: -74.0060152, eventType: 1 },
-                { user_id: 1, name: "Test Event 3", description: "This is a description.", eventDate: "2020-12-27 18:00:00", location: "New York", latitude: 40.7127281, longitude: -74.0060152, eventType: 0 }
+                { user_id: 2, name: "Test Event 1", description: "This is a description.", eventDate: "2020-12-25 18:00:00", location: "Guildford", latitude: 51.2356068, longitude: -0.5732063, eventType: 1 },
+                { user_id: 2, name: "Test Event 2", description: "This is a description.", eventDate: "2020-12-26 18:00:00", location: "New York", latitude: 40.7127281, longitude: -74.0060152, eventType: 1 },
+                { user_id: 2, name: "Test Event 3", description: "This is a description.", eventDate: "2020-12-27 18:00:00", location: "New York", latitude: 40.7127281, longitude: -74.0060152, eventType: 0 }
               ])
 Ticket.create!([
                  { event_id: 1, price: 50.0, name: "Ticket 1", description: "Test description", quantity: 20 },
