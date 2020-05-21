50349f1fb1e5db0cc048f35d13b6eaa1f7cbe31b
hl00901 <hl00901@surrey.ac.uk>
2020-05-21 20:30:23 +0100

    fixed event date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -220,6 +220,6 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.require(:event).permit(:user_id, :name, :description, :location, :eventType)
+    params.require(:event).permit(:user_id, :name, :description, :location, :eventType, :eventDate)
   end
 end
