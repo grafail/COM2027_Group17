1b225fca27b4eebb2cd66baba7424552dfd68622
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-05-23 21:50:57 +0300

    Added exchange rates (they refresh automatically every hour beginning 10 seconds after run)

---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,15 @@
+#
+# config/initializers/scheduler.rb
+require 'rufus-scheduler'
+
+# Let's use the rufus-scheduler singleton
+#
+s = Rufus::Scheduler.singleton
+
+# Awesome recurrent task...
+#
+
+s.every '1h', :first_at => Time.now + 10 do
+  MoneyRails.default_bank.update_rates
+  Rails.logger.info "Currency exchange rates were updated!"
+end
