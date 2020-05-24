202ac45ae8492ca60ea8a7001a086e5a9fb78064
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-05-24 13:48:23 +0300

    Show message after contact

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,13 @@ class HomeController < ApplicationController
 
   end
 
-  def contact; end
+  def contact
+  end
+
+  def request_contact
+    ## In an actual application an email would be send to the administrators here
+    redirect_to root_path, notice: 'Message submitted successfully!'
+  end
 
   def about; end
   
