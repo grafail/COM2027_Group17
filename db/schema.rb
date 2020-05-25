2f4e6c23ffa870d26ccd0dbef80d33f9ad0017b6
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-05-25 22:34:59 +0300

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,19 @@ ActiveRecord::Schema.define(version: 2020_05_24_121544) do
     t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'String' for column 'firstName'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "isBusiness", default: false, null: false
+    t.string "firstName", default: "", null: false
+    t.string "lastName", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
